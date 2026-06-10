021729a221170882f9b555cb788c2a35803921da
Shahriyar-Shazib <shazib@analyzenbd.com>
2026-06-10 20:28:45 +0600

    update

--- next.config.js
+++ next.config.js
@@ -27,20 +27,20 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio',
+
   images: {
-    unoptimized: true, // <-- Add this line
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
       { protocol: 'https', hostname: 'github.com' },
       { protocol: 'https', hostname: 'ghchart.rshah.org' },
     ],
   },
-  output: 'export',
-  trailingSlash: true,
-  // IMPORTANT: If your GitHub repo is named "portfolio", your URL will be username.github.io/portfolio.
-  // In that case, you MUST change basePath to '/portfolio'.
-  // If your repo is named "username.github.io", leave it empty ('').
-  basePath: '', 
 };
 
 module.exports = nextConfig;
